1043a42045c0b3524c845f6c8e02deaea961e6ef
Linh Hoang <ellehoang@hotmail.com>
2021-10-28 19:56:28 -0400

    findKeyByValue function takes object and value and returns first key containing given value

---
+++ findKeyByValue.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🚨🚨🚨 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// function findKeyByValue takes object & value
+const findKeyByValue = function(obj, val) {
+// scan object
+  for (let key of Object.keys(obj)) {
+    // return first key containing given value
+    if (obj[key] === val) {
+      return key;
+    }
+  }
+  // no key w/ given value is found, return undefined
+  return undefined;
+};
+
+
+
+// TEST CASE
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
