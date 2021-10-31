d449d3b1a2cc3e0a873e1f2430e712449f50d468
Linh Hoang <ellehoang@hotmail.com>
2021-10-31 16:04:41 -0400

    Implement AssertArrasyEqual and linting to takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,20 @@
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actualArrs, expectedArrs) {
+  if (eqArrays(actualArrs, expectedArrs) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${actualArrs} === ${expectedArrs}`);
+  } else {
+    console.log(`🚨🚨🚨 Assertion Failed: ${actualArrs} !== ${expectedArrs}`);
+  }
+};
+
 // takeUntil function takes 2 PARAMETERS: 1. array to work with; 2. callback
 // callback should only be provided 1 VALUE: item in array
 // will keep collecting items from array until callback returns truthy value
@@ -5,7 +22,7 @@
 const grocery = ["Milk", "Carrot", "Onion", "Bread", "Beef", "Corn", "Rice"];
 
 const takeUntil = function(array, callback) {
-  let bag =[];
+  let bag = [];
   for (let item of array) {
     if (callback(item)) {
       console.log(`Your items of: ${bag}  have been bagged.`);
@@ -15,7 +32,7 @@ const takeUntil = function(array, callback) {
     }
   }
 };
-takeUntil(grocery, item => item === "Beef");
+assertArraysEqual(takeUntil(grocery, item => item === "Beef"), ["Milk", "Carrot", "Onion", "Bread"]);
 
 // return "slice of array w/ elements taken from beginning"
 // should keep going until callback/predicate return truthy value
