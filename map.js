5805c27a843135103af928cbb740b6789386dffa
Linh Hoang <ellehoang@hotmail.com>
2021-10-30 21:27:55 -0400

    impletementing map funtion starting with empty arrayin map.js

---
+++ map.js
@@ -0,0 +1,13 @@
+// map function takes TWO ARGs: 1. array to map; 2. Callback function
+//return new array based on result of callback function
+
+const words = ["gound", "control", "to", "major", "tom"];
+
+/*RefError: map is not defined
+const results1 = map(words, word => word[0]);
+console.log(results1);
+*/
+const map = function() {
+  const results = [];
+  return results;
+};
