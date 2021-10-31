9478f9e24cf993860a96afda7b1044ef484a5c87
Linh Hoang <ellehoang@hotmail.com>
2021-10-30 20:59:54 -0400

    Add comments and spacing cleanup

--- eqObjects.js
+++ eqObjects.js
@@ -9,7 +9,7 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
-  if (arr1.length === arr2.length) {
+  if (arr1.length === arr2.length) { // comparing two arrays
     for (let i = 0; i < arr1.length; i++) {
       if (arr1[i] !== arr2[i]) {
         return false;
@@ -26,7 +26,7 @@ const eqObjects = function(object1, object2) {
 // 2 objects equal when have same num of keys
 // value for each key in object1 = value for each key in object2
 // use eqArrays when there is array value encountered for key
-  if (Object.keys(object1).length === Object.keys(object2).length) {
+  if (Object.keys(object1).length === Object.keys(object2).length) { // compares length of returned array of objects keys
     for (let key of Object.keys(object1)) {
       if (Array.isArray(object1[key])) { // if object key has array
         if (eqArrays(object1[key], object2[key])) { // compare arrays
@@ -50,6 +50,7 @@ const eqObjects = function(object1, object2) {
   }
 };
 // return true or false, based on perfect match(identical
+
 // TEST CASE
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
