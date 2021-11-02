ad9ac40c4ad636404ea4329cb48813ae195d1594
Linh Hoang <ellehoang@hotmail.com>
2021-11-02 08:35:30 -0400

    reforrmat test codes for all to pass assertEqual function and implement head module to import

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,6 +1,7 @@
 const assertEqual = require('../assertEqual'); // import assertEqual function
+const head = require('../head');
 
-assertEqual(head([3, 4, 5]), 5);
-assertEqual(head([]), 2);
+assertEqual(head([3, 4, 5]), 3);
+assertEqual(head([]), undefined);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head(["Labs"]), "Labs");
