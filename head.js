f662944d07bca9cd25655b3bb97517b7f1c2e00b
Linh Hoang <ellehoang@hotmail.com>
2021-11-01 22:12:18 -0400

    move test codes to assertEqualTest, remove assertEqual import code, and add export code

--- head.js
+++ head.js
@@ -1,9 +1,4 @@
-const assertEqual = require('./assertEqual'); // import assertEqual assertion code
-
 const head = function(arr) {
   return arr[0];
 };
-assertEqual(head([3, 4, 5]), 5); // failed assertion b/c first element is not expected.
-assertEqual(head([]), 2); // failed assertion b/c empty array returns undefined as its head
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); //pass assertion b/c array returns expected element
-assertEqual(head(["Labs"]), "Labs"); // pass assertion b/c array has only one element that is the expected element
+module.exports = head; // export to headTest.js
