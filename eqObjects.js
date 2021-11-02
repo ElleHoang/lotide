66598b947987ef957b0deeeca2d85e8fbc55d7f2
Linh Hoang <ellehoang@hotmail.com>
2021-11-02 15:31:41 -0400

    remove test case to transfer over to eqObjectsTest.js

--- eqObjects.js
+++ eqObjects.js
@@ -23,19 +23,3 @@ const eqObjects = function(object1, object2) {
     console.log("False: no key match");
   }
 };
-
-
-// TEST CASE
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-eqObjects(ab, ba); // True
-
-const abc = { a: "1", b: "2", c: "3" };
-eqObjects(ab, abc); // False
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-eqObjects(cd, dc); // True
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-eqObjects(cd, cd2); // False
