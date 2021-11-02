818dfcdf523fab946cfac1d0a94a6f5bc12c3172
Linh Hoang <ellehoang@hotmail.com>
2021-11-02 15:38:51 -0400

    lint and add comment

--- eqObjects.js
+++ eqObjects.js
@@ -4,14 +4,16 @@ const eqArrays = require('./eqArrays');
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length === Object.keys(object2).length) {
     for (let key of Object.keys(object1)) {
-      if (Array.isArray(object1[key])) { 
+      if (Array.isArray(object1[key])) {
         if (eqArrays(object1[key], object2[key])) {
+          // continue
         } else {
           console.log("False: no key match");
           return false;
         }
       } else {
-        if (assertEqual(object1[key], object2[key])) { 
+        if (assertEqual(object1[key], object2[key])) {
+          // continue
         } else {
           console.log("False: no key match");
           return false;
