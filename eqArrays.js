3cecc4a80f0fefac153fd604ed060b5bd9b5f354
Linh Hoang <ellehoang@hotmail.com>
2021-11-02 08:20:37 -0400

    Remove aeqArrays test codes to add into eqArraysTest

--- eqArrays.js
+++ eqArrays.js
@@ -7,9 +7,3 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 module.exports = eqArrays;
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays([], []), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
