e9e1ec8996cedf6f45ecefc844ddae76dce6b453
Linh Hoang <ellehoang@hotmail.com>
2021-11-02 19:00:24 -0400

    implement all lotide module imports and module export object

---
+++ index.js
@@ -0,0 +1,33 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const assertEqual = require('./assertEqual');
+const eqObjects = require('./eqObjects');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  assertEqual: assertEqual,
+  eqObjects: eqObjects,
+  assertArraysEqual: assertArraysEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  letterPositions: letterPositions,
+  map: map,
+  takeUntil: takeUntil,
+  without: without
+};
